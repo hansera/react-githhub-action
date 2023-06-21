d23a95a1fe472290ff5f25ed5413818f38f5e79a
Sera Han <hsr0782@gmail.com>
2023-06-21 09:59:14 +0900

    initial

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,43 @@
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('the counter starts at 0', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const counterElemnet = screen.getByTestId("counter");
+  expect(counterElemnet).toHaveTextContent(0);
 });
+
+test('minus button has correct text', () => {
+  render(<App />);
+
+  const buttonElemnet = screen.getByTestId("minus-button");
+  expect(buttonElemnet).toHaveTextContent("-")
+})
+
+test('plus button has correct text', () => {
+  render(<App />);
+  const buttonElemnet = screen.getByTestId("plus-button");
+  expect(buttonElemnet).toHaveTextContent("+");
+})
+
+test('when the + button is pressed, the counter changes to 1',() => {
+render(<App />);
+const buttonElemnet = screen.getByTestId("plus-button")
+fireEvent.click(buttonElemnet);
+const counterElemnet = screen.getByTestId("counter");
+expect(counterElemnet).toHaveTextContent(1);
+})
+
+test('on/off button has blue color', () => {
+  render(<App />);
+  const buttonElemnet = screen.getByTestId("on/off-button");
+  expect(buttonElemnet).toHaveStyle({backgroundColor: 'blue'});
+})
+
+test("Prevent the -,+ button from being pressed when the on/off button is clicked", () => {
+  render(<App />);
+  const onOffButtonElement = screen.getAllByTestId("on/off-button");
+  fireEvent.click(onOffButtonElement);
+  const plusButtonElement = screen.getByTestId("plus-button");
+  expect(plusButtonElement).toBeDisabled();
+})
